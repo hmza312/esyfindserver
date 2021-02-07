110f9ee249f57d522dd1e71c2611a17db8714c14
hamza <hmza312@users.noreply.github.com>
2021-02-07 13:01:40 +0500

    dh

---
+++ config/config.js
@@ -0,0 +1,18 @@
+const config={
+    production :{
+        SECRET: process.env.SECRET,
+        DATABASE: process.env.MONGODB_URI
+    },
+    default : {
+        SECRET: 'mysecretkey',
+        // DATABASE: 'mongodb://localhost:27017/Users'
+        DATABASE: 'mongodb+srv://maryam:maryam@cluster0.812je.mongodb.net/<dbname>?retryWrites=true&w=majority'
+
+
+    }
+}
+
+
+exports.get = function get(env){
+    return config[env] || config.default
+}
